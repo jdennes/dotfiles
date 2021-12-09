43b144dec94f7003173586b9e5ee53e1cadbc049
James Dennes <jdennes@gmail.com>
2021-12-09 11:32:04 +0100

    symlink with -f

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-ln -s $(pwd)/gitconfig $HOME/.gitconfig
-ln -s $(pwd)/gitignore $HOME/.gitignore
-ln -s $(pwd)/vimrc $HOME/.vimrc
-ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -sf $(pwd)/gitconfig $HOME/.gitconfig
+ln -sf $(pwd)/gitignore $HOME/.gitignore
+ln -sf $(pwd)/vimrc $HOME/.vimrc
+ln -sf $(pwd)/tmux.conf $HOME/.tmux.conf
